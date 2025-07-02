d2fed96f90ce1a867e12505efb1551ee8e79d27a
Om Shukal <omshukal12@gmail.com>
2025-07-02 21:36:46 +0530

    add method to print pattern of asterisks in Array class

--- Array.java
+++ Array.java
@@ -214,10 +214,22 @@ public class Array {
 //         return arr;
 //     }
 
-            public static void main(String[] args){
-            int arr[] = {1,2,3,4,5,6,7,8};
+    public static void ptn() {
+        for(int i = 1; i<=10 ; i++){
+            for(int j = 1; j<=i;j++){
+                
+                System.out.print("*");
+            }
+        
+             System.out.println(" ");
+        }
+    }
 
-           
+
+            public static void main(String[] args){
+            // int arr[] = {1,2,3,4,5,6,7,8};
+
+           ptn();
            
            
     }
