e0b5844c7d90866b026be305b91215573d40dcd4
Om Shukal <omshukal12@gmail.com>
2025-07-26 22:21:03 +0530

    todays practice

---
+++ Loops/Pattern2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pattern2 {
+
+    //  for(int i=n;i>=1;i--){
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       for(int i=1;i<=n;i++){
+        // loop for spaces
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        // loop for stars
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+    }
+}
