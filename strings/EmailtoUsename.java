7995037553720cec3a41aeecccb6558374a24369
Om Shukal <omshukal12@gmail.com>
2025-08-13 23:01:44 +0530

    Add EmailtoUsename class to extract username from email input

---
+++ strings/EmailtoUsename.java
@@ -0,0 +1,20 @@
+// package strings;
+
+import java.util.Scanner;
+
+public class EmailtoUsename {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String email = sc.nextLine();
+    String username= " ";
+
+    for(int i =0;i<email.length();i++){
+        if(email.charAt(i)=='@'){
+            break;
+        }else{
+            username += email.charAt(i);
+        }
+    }
+    System.out.println(username);
+}
+}
